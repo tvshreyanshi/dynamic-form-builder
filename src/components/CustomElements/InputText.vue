c8e3045bc67e16c663a5e2bf8d6fb86d92e08076
tv-shreyanshi <shreyanshi@techvoot.com>
2024-04-09 10:38:50 +0530

    create input-first

---
+++ src/components/CustomElements/InputText.vue
@@ -0,0 +1,32 @@
+<template>
+    <input
+    :id="id"
+    :type="type"
+    :min="min"
+    :max="max"
+    :step="step"
+    :value="value"
+    :placeholder="placeholder"
+    @input="update"
+    size="sm"
+    :data-vv-name="id"
+     />
+
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    props: ({
+        type: null,
+        min: null,
+        max: null,
+        step: null,
+        rules: null,
+        placeholder: null,
+    }),
+    setup() {
+        
+    },
+})
+</script>
---
+++ src/components/Dashboard/MainLayout.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>Main Layout--------------------------------------</div>
+    <div>Main Layout--------------------------------------</div>
+    <div>Main Layout--------------------------------------</div>
+    <div>Main Layout--------------------------------------</div>
+    <div>Main Layout--------------------------------------</div>
+    <div>Main Layout--------------------------------------</div>
+    <div>Main Layout--------------------------------------</div>
+    <div>Main Layout--------------------------------------</div>
+    <div>Main Layout--------------------------------------</div>
+    <div>Main Layout--------------------------------------</div>
+</template>
+<script>
+import { defineComponent } from 'vue'
+// import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
