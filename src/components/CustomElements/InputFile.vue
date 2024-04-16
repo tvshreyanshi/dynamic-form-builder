8a305ed2bbd7cd68edb0aa2ac6205f4b00262386
tv-shreyanshi <shreyanshi@techvoot.com>
2024-04-16 10:34:06 +0530

    16-04

---
+++ src/components/CustomElements/InputFile.vue
@@ -0,0 +1,93 @@
+<template>
+    <div>
+        <input
+        :id="id"
+        :type="'file'"
+        :step="step"
+        :placeholder="placeholder"
+        :state="errors.has(this.id) ? 'invalid' : ''"
+        @change="changeCall"
+        v-validate="rules"
+        :data-vv-name="id"
+        :data-vv-as="label"
+        data-vv-delay="800"
+        :class="['mt-3 d-none']"
+         />
+    </div>
+</template>
+<script>
+import { computed, defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'InputFile',
+    props: ({
+    type: null,
+    min: null,
+    max: null,
+    step: null,
+    rules: null,
+    placeholder: null,
+    isMultiple: null,
+    labelColMd: {
+        type: Number,
+        default: 3,
+      },
+      labelColLg: {
+        type: Number,
+        default: 4,
+      },
+      inputColMd: {
+        type: Number,
+        default: 9,
+      },
+      inputColLg: {
+        type: Number,
+        default: 8,
+      },
+      simple: {
+        type: Boolean,
+        default: false,
+      },
+      preventUpdate: {
+        type: Boolean,
+        default: false,
+      },
+      rowIndex: null,
+      id: null,
+      label: null,
+      model: null,
+      module: null,
+      index: null,
+      value: null,
+      help: null,
+      hideField: {
+        type: Boolean,
+        default: false,
+      },
+  }),
+    setup() {
+        const getFileInput = computed(() => {
+            if (this.value) {
+            const path = this.value;
+            const getfileName = path.split('/').pop().split('-').pop();
+            return getfileName;
+        }
+        return '';
+        });
+        const changeCall = (e) => {
+            const file = e.target.files;
+            this.selectedFile = file[0].name;
+            this.update({ id: this.id, value: this.isMultiple ? file[0] : file[0] });
+        }
+        const clickFile = (fileInputId) => {
+            document.getElementById(fileInputId).click();
+        }
+        return {
+            getFileInput,
+            changeCall,
+            clickFile,
+        }
+        
+    },
+})
+</script>
--- src/components/Dashboard/SideBar.vue
+++ src/components/Dashboard/SideBar.vue
@@ -2,9 +2,9 @@
   <!-- drawer init and show -->
   <div class="flex">
     <div class="flex top-16">
-      <button @click="closeLeftSidebar">Open sideBar</button>
+      <button @click="closeLeftSidebar"><i class="fa-solid fa-bars"></i></button>
     </div>
-    <div v-if="leftSidebarOpened" class="fixed -translate-x-full ease-in-out duration-300 top-16 left-0 z-40 w-64 h-screen p-4 overflow-y-auto transition-transform transform-none bg-white dark:bg-gray-800" tabindex="-1" aria-labelledby="drawer-navigation-label">
+    <div v-if="leftSidebarOpened" class=" w-72 duration-300 fixed top-16 left-0 z-40 h-screen p-4 overflow-y-auto bg-white dark:bg-gray-800" tabindex="-1" aria-labelledby="drawer-navigation-label">
       <h5  class="text-base font-semibold text-gray-500 uppercase dark:text-gray-400">Menu</h5>
       <button @click="closeLeftSidebar()" type="button" data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 end-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" >
           <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
@@ -14,10 +14,6 @@
           <ul class="space-y-2 font-medium" v-for="(sb, index) in sidebarMenu" :key="index">
             <li draggable="true" @dragstart="dragStart(sb)">
                 <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                  <!-- <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                      <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
-                      <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
-                  </svg> -->
                   <span class="ms-3">{{ sb.title }}</span>
                 </a>
             </li>
@@ -69,10 +65,10 @@
                       <p>Options</p>
                       <div class="my-2">
                         <label>Add Options</label>
-                        <b-button class="btn-sm" @click="addOption">+</b-button>
+                        <button class="ml-3 bg-white text-black p-3 h-7 items-center inline-flex rounded-md" @click="addOption">+</button>
                       </div>
                       <div v-for="(item,index) in selectedItem.options" :key="index" class="flex items-center">
-                        <input type="text" v-model="item.text" class="block my-2 w-56 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                        <input type="text" v-model="item.text" class="block my-2 w-56 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                         <button @click="removeItem(index)"><img src="../../assets/images/Icons//delete.svg" /></button>
                       </div>
                     </div>
@@ -115,6 +111,7 @@ export default defineComponent({
         title: "Checkbox",
         label: 'CheckBox',
         id: 'checkbox',
+        icon: 'fa-regular fa-square-check',
         input: 'InputCheckbox',
         options: [
           { text: 'Horizontal', value: 'HORIZONTAL' },
